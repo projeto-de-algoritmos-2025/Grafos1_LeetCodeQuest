f4e6b53edf6425f66fd64b8ac5a08c6fd45f3aba
PedroHhenriq <100311889+PedroHhenriq@users.noreply.github.com>
2025-04-16 15:49:35 -0300

    Adicionando Questão 847

---
+++ Grafos1/Questao_847/807.py
@@ -0,0 +1,30 @@
+from collections import deque 
+class Solution:
+    def shortestPathLength(self, graph):
+        n = len(graph) # Armazena o número total de nós
+        objetivo = frozenset(range(n)) # Conjutno dos nós que precisam ser visitados
+        fila = deque()
+        visitados = set() # Conjunto para armazenar os estados já explorados
+        def inicializar_estado(indice): # Inicializa a fila com cada nó como ponto de partida
+            estado = (indice, frozenset([indice])) # Nó atual
+            fila.append(estado) # Adiciona o estado a fila
+            visitados.add(estado) # Marca o estado como visitado
+        for i in range(n):
+            inicializar_estado(i) # Para cada nó, inicializa a BFS a partir dele
+        passos = 0 # Contador de passo até visitar todos os nós
+        def processar_nivel():
+            for _ in range(len(fila)):
+                no_atual, nos_visitados = fila.popleft() # Para cada estado, remove o próximo estado da fila
+                if nos_visitados == objetivo:
+                    return True # Se todos os nós foram visitados, retorna True
+                for vizinho in graph[no_atual]:
+                    novo_visitado = nos_visitados | {vizinho} # Marca o vizinho como visitado
+                    novo_estado = (vizinho, frozenset(novo_visitado)) # Novo estado a ser visitado
+                    if novo_estado not in visitados:
+                        visitados.add(novo_estado) # Marca o novo estado como visitado
+                        fila.append(novo_estado) # Adiciona o novo estado a fila
+            return False
+        while fila:
+            if processar_nivel(): # Processa o nível e verifica se todos os foram visitados
+                return passos # Retorna o número de passos se a condição for satisfeita
+            passos +=1
